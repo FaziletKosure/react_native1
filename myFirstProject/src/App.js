899e9a3dd3f0445fdf827470c5fef6323375b3a4
FaziletKosure <faziletkosure1@gmail.com>
2020-10-03 20:25:39 +0200

    app.js modified

---
+++ myFirstProject/src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App=()  => {
+  return  (
+  <View style={{
+    backgroundColor :'red',
+    margin:10,
+    padding:5
+  }}>
+    <Text>Merhaba!</Text>
+    </View>);
+    
+};
+
+export default App;
