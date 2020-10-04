a2753dc42057b7ec9b4b731d810c7b3c4e17e6dd
FaziletKosure <faziletkosure1@gmail.com>
2020-10-04 20:02:34 +0200

    app js updated

--- myFirstProject/src/App.js
+++ myFirstProject/src/App.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import MyComponent from './components/MyComponent';
 import {
   SafeAreaView,
   StyleSheet,
@@ -10,15 +11,75 @@ import {
 } from 'react-native';
 
 const App=()  => {
-  return  (
-  <View style={{
-    backgroundColor :'red',
-    margin:10,
-    padding:5
-  }}>
-    <Text>Merhaba!</Text>
-    </View>);
+  const my_name='Fazilet';
+  // function myFunction() {
+  //  return (<View>
+  //    <Text style={styles.hello}>Selam Clarusway!</Text>
+  //    <Text style={styles.hello}>Selam Clarusway!</Text>
+  //  </View>) 
     
+  // }
+  const isAdmin=true;
+  function myFunction(userName) {
+  return <Text>{userName}</Text>
+    
+    // return isAdmin ?  <Text style={{fontSize:40}}>Admin kullanicisi</Text>:null
+    // if(isAdmin)
+    //   return <Text style={{fontSize:40}}>Admin kullanicisi</Text>
+    //   return null;
+    
+      // return <Text>Uye kullanicisi</Text>
+    
+    
+  }
+   return (
+     <>
+       <View style={styles.container}>
+      <Text style={styles.text}>
+      CLARUSWAY
+    </Text>
+     </View>
+     <View style={styles.container}>
+      <Text style={styles.text}>
+      CLARUSWAY
+    </Text>
+     </View>
+    <MyComponent/>
+   {/* <Text>Hosgelgin {my_name}</Text>
+   <Text>Sonuc {5+4}</Text> */}
+  {myFunction("Fazilet")}
+  {/* { isAdmin ?  <Text style={{fontSize:40}}>Admin kullanicisi</Text>:null} */}
+  {isAdmin && <Text style={{fontSize:40}}>Admin kullanicisi</Text>}
+     </>
+   ); 
+  // (<View style={{
+  //   backgroundColor :'red',
+  //   margin:10,
+  //   padding:5
+  // }}>
+  //   <Text>Merhaba!</Text>
+  //   </View>);   
 };
+const styles=StyleSheet.create({
+  container:{
+    backgroundColor:'purple',
+    margin:20,
+    padding:10,
+    borderRadius:5,
+
+  },
+  text:{
+    color:'white',
+    fontWeight:'bold',
+    fontSize:25,
+    textAlign:'center',
+  },
+  hello:{
+    margin:10,
+    textAlign:'center',
+    fontSize:25
+  }
+
+});
 
 export default App;
