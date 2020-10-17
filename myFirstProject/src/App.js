b76216698e1f00e38eedae6c01250407deed7388
FaziletKosure <faziletkosure1@gmail.com>
2020-10-17 20:11:09 +0200

    add productcard

--- myFirstProject/src/App.js
+++ myFirstProject/src/App.js
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import MyComponent from './components/MyComponent';
+import Function from './components/Function';
 import {
   SafeAreaView,
   StyleSheet,
@@ -10,79 +10,72 @@ import {
   StatusBar,
 } from 'react-native';
 
-const App=()  => {
-  const my_name='Fazilet';
+const App = () => {
+  const my_name = 'Fazilet';
   // function myFunction() {
   //  return (<View>
   //    <Text style={styles.hello}>Selam Clarusway!</Text>
   //    <Text style={styles.hello}>Selam Clarusway!</Text>
-  //  </View>) 
-    
+  //  </View>)
+
   // }
-  const isAdmin=true;
+  const isAdmin = true;
   function myFunction(userName) {
-  return <Text>{userName}</Text>
-    
+    return <Text>{userName}</Text>;
+
     // return isAdmin ?  <Text style={{fontSize:40}}>Admin kullanicisi</Text>:null
     // if(isAdmin)
     //   return <Text style={{fontSize:40}}>Admin kullanicisi</Text>
     //   return null;
-    
-      // return <Text>Uye kullanicisi</Text>
-    
-    
+
+    // return <Text>Uye kullanicisi</Text>
   }
   const cityArray = ['Brussel', 'Ghent', 'Antwerp', 'Brugge'];
   const cities = () => cityArray.map((city) => <Text>{city}</Text>);
-   return (
-     <>
-       <View style={styles.container}>
-      <Text style={styles.text}>
-      CLARUSWAY
-    </Text>
-     </View>
-     <View style={styles.container}>
-      <Text style={styles.text}>
-      CLARUSWAY
-    </Text>
-     </View>
-    <MyComponent/>
-   {/* <Text>Hosgelgin {my_name}</Text>
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>CLARUSWAY 🎃</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.text}>CLARUSWAY</Text>
+      </View>
+      <MyComponent />
+      {/* <Text>Hosgelgin {my_name}</Text>
    <Text>Sonuc {5+4}</Text> */}
-  {myFunction("Fazilet")}
-  {/* { isAdmin ?  <Text style={{fontSize:40}}>Admin kullanicisi</Text>:null} */}
-  {isAdmin && <Text style={{fontSize:40}}>Admin kullanicisi</Text>}
-  {cities()}
-     </>
-   ); 
+      {myFunction('Fazilet')}
+      {/* { isAdmin ?  <Text style={{fontSize:40}}>Admin kullanicisi</Text>:null} */}
+      {isAdmin && <Text style={{fontSize: 40}}>Admin kullanicisi</Text>}
+      {cities()}
+      <Function />
+    </>
+  );
   // (<View style={{
   //   backgroundColor :'red',
   //   margin:10,
   //   padding:5
   // }}>
   //   <Text>Merhaba!</Text>
-  //   </View>);   
+  //   </View>);
 };
-const styles=StyleSheet.create({
-  container:{
-    backgroundColor:'purple',
-    margin:20,
-    padding:10,
-    borderRadius:5,
-
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'purple',
+    margin: 20,
+    padding: 10,
+    borderRadius: 5,
   },
-  text:{
-    color:'white',
-    fontWeight:'bold',
-    fontSize:25,
-    textAlign:'center',
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
   },
-  hello:{
-    margin:10,
-    textAlign:'center',
-    fontSize:25
-  }
-
+  hello: {
+    margin: 10,
+    textAlign: 'center',
+    fontSize: 25,
+  },
 });
 
 export default App;
--- myFirstProject/src/components/Function.js
+++ myFirstProject/src/components/Function.js
@@ -1,8 +1,20 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 const App = (props) => {
   const [counter, setCounter] = useState(0);
+  const [userName, setUserName] = useState('');
+  const [createDate, setCreateDate] = useState(new Date());
+  useEffect(() => {
+    Alert.alert('WElCOM', 'My sweet app');
+  }, []);
+  useEffect(() => {
+    if (counter == 10) {
+      Alert.alert('OOPS', '10 dan fazla tiklayamazsin');
+    }
+  }, [counter]);
+
+  console.log('RENDER');
   const countUp = () => setCounter(counter + 1);
   const countDown = () => setCounter(counter - 1);
   const countReset = () => setCounter(0);
